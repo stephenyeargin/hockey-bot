7a2290a85b72fbdf0bbf670d809b08448ba9c160
Stephen Yeargin <stephen.yeargin@gmail.com>
2024-06-29 12:37:20 -0500

    Let that fail a level up

--- src/sportsclubstats.js
+++ src/sportsclubstats.js
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import logger from './utils/logger.js';
 
 const SPORTS_CLUB_STATS_DATA_FEED = 'http://www.sportsclubstats.com/d/NHL_ChanceWillMakePlayoffs_Small_A.json';
 
@@ -14,17 +13,12 @@ const SportsClubStats = {
   getLeagueLiveOdds: async () => {
     const Teams = JSON.parse(fs.readFileSync('./src/data/teams.json', 'utf-8'));
     const leagueOdds = {};
-    try {
-      const response = await axios.get(SPORTS_CLUB_STATS_DATA_FEED);
-      response.data.data.forEach((history) => {
-        const teamCode = Teams.find((t) => history.label === t.name)?.abbreviation;
-        if (!teamCode) return;
-        leagueOdds[teamCode] = history.data[history.data.length - 1];
-      });
-    } catch (e) {
-      logger.error(e);
-      return null;
-    }
+    const response = await axios.get(SPORTS_CLUB_STATS_DATA_FEED);
+    response.data.data.forEach((history) => {
+      const teamCode = Teams.find((t) => history.label === t.name)?.abbreviation;
+      if (!teamCode) return;
+      leagueOdds[teamCode] = history.data[history.data.length - 1];
+    });
     return leagueOdds;
   },
 };
