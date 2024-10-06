f23e13a44ffeb3e2ceef347f7e8a4c24693473c6
Stephen Yeargin <stephen@yearg.in>
2024-10-05 19:45:20 -0500

    Fix NaN bug, remove Sports Club Stats

--- src/utils/image-generator.test.js
+++ src/utils/image-generator.test.js
@@ -15,40 +15,6 @@ describe('Mastodon', () => {
   describe('generateLeaguePlayoffOddsImage', () => {
     it('should generate an image', async () => {
       const standings = await JSON.parse(fs.readFileSync('./src/test/fixtures/standings.json', 'utf-8'));
-      const sportsClubStatsOdds = {
-        ANA: 0.0021,
-        UTA: 0.014,
-        BOS: 99.97,
-        BUF: 0.84,
-        CAR: 99.64,
-        CBJ: 0.0083,
-        CGY: 16.57,
-        CHI: 0,
-        COL: 98.88,
-        DAL: 99.92,
-        DET: 96.08,
-        EDM: 98.14,
-        FLA: 99.99,
-        LAK: 92.04,
-        MIN: 18.62,
-        MTL: 0.0094,
-        NJD: 12.97,
-        NSH: 50.52,
-        NYI: 8.1,
-        NYR: 99.99,
-        OTT: 0.19,
-        PHI: 59.31,
-        PIT: 44.01,
-        SEA: 11.06,
-        SJS: 0.00018,
-        STL: 18.88,
-        TBL: 74.35,
-        TOR: 98.91,
-        VAN: 99.99,
-        VGK: 95.4,
-        WPG: 99.97,
-        WSH: 5.65,
-      };
       const moneyPuckOdds = {
         ANA: 0,
         UTA: 0.09,
@@ -87,7 +53,6 @@ describe('Mastodon', () => {
       const image = await generateLeaguePlayoffOddsImage({
         standings: standings.standings,
         odds: {
-          SCS: sportsClubStatsOdds,
           MP: moneyPuckOdds,
         },
         updatedAt,
@@ -103,7 +68,6 @@ describe('Mastodon', () => {
 
   describe('generateTeamPlayoffOddsImage', () => {
     it('should generate an image', async () => {
-      const sportsClubStatsOdds = 50.52;
       const moneyPuckOdds = 49.57;
       const updatedAt = 'March 9, 2024 9:03 ET';
       const image = await generateTeamPlayoffOddsImage({
@@ -114,7 +78,6 @@ describe('Mastodon', () => {
           teamColor: '#FFB81C',
         },
         odds: {
-          'Sports Club Stats': sportsClubStatsOdds,
           MoneyPuck: moneyPuckOdds,
         },
         updatedAt,
